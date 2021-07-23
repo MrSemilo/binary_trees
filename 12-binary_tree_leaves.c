7eb4162370e5b673324ec22661bf99e87d9f7c86
samilo-del <camilovelenciapadilla@gmail.com>
2021-07-23 04:13:51 +0000

    add

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_leaves -  prototype
+ * @tree: root node of the tree to count the number of leaves
+ * Return: return
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t left_leaves = 0, right_leaves = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_leaves = binary_tree_leaves(tree->left);
+	right_leaves = binary_tree_leaves(tree->right);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (left_leaves != 0 || right_leaves != 0)
+		return (left_leaves + right_leaves);
+
+	return (0);
+}
